2dd367348abe4f0f95f71eb345f4388ad09cb337
Hanna <hanna.milk@khk.ee>
2021-10-14 09:43:31 +0300

    teksti väljastamine ja kommenteerimine
    muutujate deklareerimine, väärtuse omistamine ja kasutamine

--- app.js
+++ app.js
@@ -2,3 +2,29 @@
 // kommentaaride kasutamine
 
 //üherealine kommentaar
+
+/*mitme
+realine
+kommentaar
+ */
+
+console.log("Tere Maailm")
+console.log("See on teine rida")
+
+console.log("See " + "on " + "liitlause ")
+console.log("Aastal 2016 valiti Eesti presidentiks Kersti Kaljulaid")
+
+
+//muutujad ja andmetüübid
+// 1. deklareerimine
+// let muutujanimi
+// 2. väärtuse omistamine
+// muutujanimi = väärtus
+
+let aasta
+aasta = 2016
+
+let president
+president= "Kersti Kaljulaid"
+
+console.log(aasta + " . Eesti presidendiks valiti " + president)
