50656c19178bec437d895a4135635b97efae0f89
Hanna <hanna.milk@khk.ee>
2021-10-28 10:55:42 +0300

    Muutujad (var), let ja const const objekt vs array

---
+++ muutujad.js
@@ -0,0 +1,36 @@
+// Muutujad
+
+// (var), let (annab v6imaluse muuta v22rtust), const (sisu on konstantne)
+
+/* let name;
+name="Kadi Tamm"
+console.log(name);
+
+let age = 15;
+console.log(age);*/
+
+/* n8 vigane const n2ide. p6hjendus on all.
+const name = "Kadi Tamm";
+console.log(name);
+//Kui const puhul lisada uus v22rtus, n2itab consoleis errorit, sest const v22rtus on konstantne.
+Nt j2rgnev v22rtus ei t88ta consoles. n2itab errorit.
+name = "Kadi Lepp"
+*/
+
+/* OBJEKTID
+const person = {
+    name: "Kadi Tamm",
+    age: 15
+} ;
+
+//katsetan kas muutuja v22rtust saab nyyd muuta//
+person.name = "Kadi Lepp"
+console.log(person)
+console.log(person.name)
+console.log(person.age)
+ */
+
+//Massiivid
+const numbers = [1, 2, 3, 4];
+numbers.push(5);
+console.log(numbers);
