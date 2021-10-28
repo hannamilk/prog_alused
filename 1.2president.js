379577d45d8427e1d6bcf5672eb7eb385caffdeb
Hanna <hanna.milk@khk.ee>
2021-10-28 18:17:38 +0300

    Parandused

--- 1.2president.js
+++ 1.2president.js
@@ -2,4 +2,5 @@ let aasta = 2016;
 let president = " Kersti Kaljulaid"
 let lause_keskosa = ". aastal valiti Eesti presidendiks"
 let lause = String(aasta) + lause_keskosa + president;
+
 console.log(lause)
