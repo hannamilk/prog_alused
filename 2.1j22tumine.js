4988c8f000cb6ddb88b75f2ca612a0d488848dd5
Hanna <hanna.milk@khk.ee>
2021-11-03 23:27:50 +0200

    j22tumise ülesanne

---
+++ 2.1j22tumine.js
@@ -0,0 +1,13 @@
+let temperatuur = prompt("Sisestage õhutemperatuur: ");
+// tingimuslause
+if(temperatuur > 4.0) {
+    console.log("J22tumisohtu ei ole")
+    alert("Väljas ei ole jäätumisohtu")
+}
+
+if(temperatuur <= 4.0) {
+    console.log("On j22tumisoht")
+    alert("Väljas on jäätumisoht")
+}
+
+//v6i kirjutada lihtsalt else { console.log(on jaatumisoht)}
