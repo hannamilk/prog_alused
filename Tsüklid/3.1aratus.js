f3c224a02f663da2e3af24649a815c4a3a9968f2
Hanna <hanna.milk@khk.ee>
2021-11-07 18:08:03 +0200

    for tsükliga korduvad tegevused

---
+++ Tsüklid/3.1aratus.js
@@ -0,0 +1,6 @@
+// kasutajasisend
+let kordus = prompt("Mitu korda peab äratus käima?");
+// for tsykkel
+for(let kord = 1; kord <= kordus; kord++) {
+    console.log(kord + ". kord Tõuse ja sära");
+}
